d53b64750c4e97b7c8f4e44d13102031970a11cd
JEON Se-Ok <33389418+seokjeon@users.noreply.github.com>
2026-01-21 20:24:31 +0900

    feat: add command exec time duration

--- experiment/scripts/linevul/run.sh
+++ experiment/scripts/linevul/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # 1. train_val + test 병합
-docker exec linevul bash -c "cat /app/RealVul/Dataset/VP-Bench_Train_Dataset/Real_Vul_data.csv > /app/RealVul/Dataset/Real_Vul_data.csv && tail -n +2 /app/RealVul/Dataset/VP-Bench_Test_Dataset/Real_Vul_data.csv >> /app/RealVul/Dataset/Real_Vul_data.csv"
+time docker exec linevul bash -c "cat /app/RealVul/Dataset/VP-Bench_Train_Dataset/Real_Vul_data.csv > /app/RealVul/Dataset/Real_Vul_data.csv && tail -n +2 /app/RealVul/Dataset/VP-Bench_Test_Dataset/Real_Vul_data.csv >> /app/RealVul/Dataset/Real_Vul_data.csv"
+echo "1. train_val + test 병합 완료."
 
-# 2. Jasper 데이터셋의 pickle 생성 (--prepare_dataset)
-docker exec linevul python /app/RealVul/Experiments/LineVul/line_vul.py \
+# 2. 데이터셋의 pickle 생성 (--prepare_dataset)
+time docker exec linevul python /app/RealVul/Experiments/LineVul/line_vul.py \
     --dataset_csv_path /app/RealVul/Dataset/Real_Vul_data.csv \
     --dataset_path /app/RealVul/Dataset/ \
     --output_dir /app/RealVul/Experiments/LineVul \
@@ -13,21 +14,23 @@ docker exec linevul python /app/RealVul/Experiments/LineVul/line_vul.py \
     --per_device_eval_batch_size 8 \
     --num_train_epochs 10 \
     --prepare_dataset
+echo "2. 데이터셋의 pickle 생성 완료."
 
-# 3. Jasper 데이터셋으로 학습 (--train)
-docker exec linevul python /app/RealVul/Experiments/LineVul/line_vul.py \
+# 3. 데이터셋으로 학습 (--train)
+time docker exec linevul python /app/RealVul/Experiments/LineVul/line_vul.py \
     --dataset_csv_path /app/RealVul/Dataset/Real_Vul_data.csv \
     --dataset_path /app/RealVul/Dataset/ \
     --output_dir /app/RealVul/Experiments/LineVul \
     --tokenizer_name microsoft/codebert-base \
     --model_name /app/RealVul/Experiments/LineVul/best_model \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
+    --per_device_train_batch_size 64 \
+    --per_device_eval_batch_size 64 \
     --num_train_epochs 10 \
     --train
+echo "3. 학습 완료."
 
-# 4. VP-Bench 테스트 데이터셋으로 테스트 (--test_predict)
-docker exec linevul python /app/RealVul/Experiments/LineVul/line_vul.py \
+# 4. 테스트 데이터셋으로 테스트 (--test_predict)
+time docker exec linevul python /app/RealVul/Experiments/LineVul/line_vul.py \
     --dataset_csv_path /app/RealVul/Dataset/Real_Vul_data.csv \
     --dataset_path /app/RealVul/Dataset/ \
     --output_dir /app/RealVul/Experiments/LineVul \
@@ -37,3 +40,4 @@ docker exec linevul python /app/RealVul/Experiments/LineVul/line_vul.py \
     --per_device_eval_batch_size 8 \
     --num_train_epochs 10 \
     --test_predict
+echo "4. 테스트 완료."
